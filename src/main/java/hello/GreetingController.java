dc0cd5883707db8d7f893d1c08e99774483ac878
viki9698 <rui.zhang9698@gmail.com>
2015-03-18 18:22:11 +0800

    new conftent

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,7 +1,9 @@
 package hello;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,36 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private static AtomicLong counter = new AtomicLong();
 	
+	@Autowired
+	private CustomerRepository repository;
+	
 	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name",defaultValue="world") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public List<Customer> greeting(@RequestParam(value="name",defaultValue="world") String name) {
+		//return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		repository.deleteAll();
+
+		// save a couple of customers
+		repository.save(new Customer("Alice", "Smith"));
+		repository.save(new Customer("Bob", "Smith"));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByFirstName("Alice"));
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+		return repository.findAll();
 	}
 }
